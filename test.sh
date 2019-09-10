57c3a4eac8fa9eb57fdd959caf897c79c94e6ffa
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2019-09-10 03:16:37 +0100

    Hello world

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+branch="$(git rev-parse --abbrev-ref HEAD)"
+
+# get computer name to append in Chime message
+username=$USER
+
+# a branch name where you want to prevent git push. In this case, it's "master"
+if [ "$branch" = "master" ]; then
+  echo "You can't commit directly to '"${branch}"' branch"
+  exit 1
+fi
